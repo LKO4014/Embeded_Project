fdbf28079d440a1df61a72d0ccdeb0d10211b33d
Koeun Lee <dlrhdms9972@gmail.com>
2025-04-09 15:24:20 +0900

    중간 세이브 _ 현재 딜레이 30/ 스탑 80 최적해

--- SEMI_1/newShield/newShield.ino
+++ SEMI_1/newShield/newShield.ino
@@ -5,14 +5,15 @@
 // 모터드라이버 PWR 
 #define motorR_F 6   //A-1A (PWM)
 #define motorR_B 7 //A-1B
-#define motorL_F 4    //B-1A (PWM)
-#define motorL_B 5  //B-1B
+#define motorL_F 5    //B-1A (PWM)
+#define motorL_B 4  //B-1B
 
 // 인식률을 위한 딜레이
 #define delay_front 50
 #define delay_end 50
 
-int default_speed = 120;
+int default_speedR = 150;
+int default_speedL = 140;
 
 void set_motorR_speed(int n);
 void set_motorL_speed(int n);
@@ -38,59 +39,58 @@ void setup() {
 bool goLeft, goRight;
 
 void loop() {
+  goLeft = digitalRead(RIGHT);
+  goRight =digitalRead(LEFT);
 
-  goLeft = digitalRead(LEFT);
-  goRight =digitalRead(RIGHT);
+  if(goLeft==HIGH && goRight ==LOW){
 
-  if(goLeft==HIGH){
     turnL();
   }
   
-  else if(goRight==HIGH){
+  else if(goRight==HIGH && goLeft ==LOW){
     turnR();
   }
-
-  if(goLeft == LOW && goRight == LOW){
+  else{
     goStr();
   }
+  delay(30);
+  stop(80);
+
 }
 
 void turnR(){
-  analogWrite(motorL_F, default_speed);
-  analogWrite(motorL_B, 0);
-
+  analogWrite(motorL_F, default_speedL);
   analogWrite(motorR_F,0);
-  analogWrite(motorR_B,default_speed);
+  analogWrite(motorL_B, 0);
+  analogWrite(motorR_B,0);
 }
 
 void turnL(){
   analogWrite(motorL_F, 0);
-  analogWrite(motorL_B, default_speed);
-
-  analogWrite(motorR_F,default_speed);
+  analogWrite(motorR_F,default_speedR);
+  analogWrite(motorL_B,0);
   analogWrite(motorR_B,0);
 }
 
 void goStr(){
-  analogWrite(motorL_F, default_speed);
+  analogWrite(motorL_F, default_speedL);
   analogWrite(motorL_B, 0);
-
-  analogWrite(motorR_F,default_speed);
+  analogWrite(motorR_F,default_speedR);
   analogWrite(motorR_B,0);
 }
 
 void stop(int time){
   analogWrite(motorL_F,0);
-  analogWrite(motorL_B,0);
   analogWrite(motorR_F,0);
+  analogWrite(motorL_B,0);
   analogWrite(motorR_B,0);
   delay(time);
 }
 
 void goBack(){
   analogWrite(motorL_F, 0);
-  analogWrite(motorL_B, default_speed);
-
+  analogWrite(motorL_B, default_speedL);
   analogWrite(motorR_F,0);
-  analogWrite(motorR_B,default_speed);
+  analogWrite(motorR_B,default_speedR);
+
 }
