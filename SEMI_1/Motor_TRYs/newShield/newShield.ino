db9d471d3c56607e32bd680220cd4dda0ae15c90
L KO <koksi9972@naver.com>
2025-04-12 15:43:59 +0900

    충돌 해결

--- SEMI_1/Motor_TRYs/newShield/newShield.ino
+++ SEMI_1/Motor_TRYs/newShield/newShield.ino
@@ -5,18 +5,23 @@
 // 모터드라이버 PWR 
 #define motorR_F 6   //A-1A (PWM)
 #define motorR_B 7 //A-1B
-#define motorL_F 4    //B-1A (PWM)
-#define motorL_B 5  //B-1B
+#define motorL_F 5    //B-1A (PWM)
+#define motorL_B 4  //B-1B
 
 // 인식률을 위한 딜레이
 #define delay_front 50
 #define delay_end 50
 
+<<<<<<< HEAD:SEMI_1/newShield/newShield.ino
 <<<<<<< HEAD
 int default_speed = 150;
 =======
 #define normal_speed 100
 >>>>>>> main
+=======
+int default_speedR = 150;
+int default_speedL = 140;
+>>>>>>> main:SEMI_1/Motor_TRYs/newShield/newShield.ino
 
 void set_motorR_speed(int n);
 void set_motorL_speed(int n);
@@ -42,10 +47,12 @@ void setup() {
 bool goLeft, goRight;
 
 void loop() {
+  goLeft = digitalRead(RIGHT);
+  goRight =digitalRead(LEFT);
 
-  goLeft = digitalRead(LEFT);
-  goRight =digitalRead(RIGHT);
+  if(goLeft==HIGH && goRight ==LOW){
 
+<<<<<<< HEAD:SEMI_1/newShield/newShield.ino
   goStr();
 /*
   if(goLeft == LOW && goRight == LOW){
@@ -66,34 +73,71 @@ void turnR(){
   analogWrite(motorR_F,0);
   analogWrite(motorL_B, 0);
   analogWrite(motorR_B,default_speed);
+=======
+    turnL();
+  }
+  
+  else if(goRight==HIGH && goLeft ==LOW){
+    turnR();
+  }
+  else{
+    goStr();
+  }
+  delay(30);
+  stop(80);
+
+}
+
+void turnR(){
+  analogWrite(motorL_F, default_speedL);
+  analogWrite(motorR_F,0);
+  analogWrite(motorL_B, 0);
+  analogWrite(motorR_B,0);
+>>>>>>> main:SEMI_1/Motor_TRYs/newShield/newShield.ino
 }
 
 void turnL(){
   analogWrite(motorL_F, 0);
+<<<<<<< HEAD:SEMI_1/newShield/newShield.ino
   analogWrite(motorR_F,default_speed);
   analogWrite(motorL_B, default_speed);
+=======
+  analogWrite(motorR_F,default_speedR);
+  analogWrite(motorL_B,0);
+>>>>>>> main:SEMI_1/Motor_TRYs/newShield/newShield.ino
   analogWrite(motorR_B,0);
 }
 
 void goStr(){
+<<<<<<< HEAD:SEMI_1/newShield/newShield.ino
   analogWrite(motorL_F, default_speed);
   analogWrite(motorR_F,default_speed);
   analogWrite(motorL_B, 0);
+=======
+  analogWrite(motorL_F, default_speedL);
+  analogWrite(motorL_B, 0);
+  analogWrite(motorR_F,default_speedR);
+>>>>>>> main:SEMI_1/Motor_TRYs/newShield/newShield.ino
   analogWrite(motorR_B,0);
 }
 
 void stop(int time){
   analogWrite(motorL_F,0);
-  analogWrite(motorL_B,0);
   analogWrite(motorR_F,0);
+  analogWrite(motorL_B,0);
   analogWrite(motorR_B,0);
   delay(time);
 }
 
 void goBack(){
   analogWrite(motorL_F, 0);
-  analogWrite(motorL_B, default_speed);
-
+  analogWrite(motorL_B, default_speedL);
   analogWrite(motorR_F,0);
+<<<<<<< HEAD:SEMI_1/newShield/newShield.ino
   analogWrite(motorR_B,default_speed);
 }
+=======
+  analogWrite(motorR_B,default_speedR);
+
+}
+>>>>>>> main:SEMI_1/Motor_TRYs/newShield/newShield.ino
